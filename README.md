04365e59b0d7f4488b0e950905131251a7e7262f
leixu <leixu@quantongfu.com>
2019-01-25 09:39:36 +0800

    java基础

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-java
-java基础
---
+++ src/conditionalStatements/Demo1.java
@@ -0,0 +1,8 @@
+package conditionalStatements;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int b = 1;
+        System.out.println(b);
+    }
+}
